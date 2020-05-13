53f5b6b04f9074fd159ec713158a7c0bf8e9ba50
Jesse Markowitz <jess.markowitz@gmail.com>
2020-05-13 01:41:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('div.target');
+}
+
+function deepestChild() {
+  var grandNode = document.getElementById('grand-node');
+  var deepNode = grandNode.children[0];
+
+  while (deepNode) {
+    grandNode = deepNode;
+    deepNode = grandNode.children[0]
+  }
+  return grandNode
+}
